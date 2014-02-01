cdd50dad32ddefcd3961252be89925e163809b39
Alexandr Plesovskikh <aplesovskih@naumen.ru>
2014-02-02 02:00:46 +0600

    its work!

--- justify.py
+++ justify.py
@@ -1,11 +1,50 @@
 #!/usr/bin/env python3
 
+import re
 import sys
 from argparse import ArgumentParser
 
 
 def justify(text, width):
-    pass
+    TEXT_INDENT = '    '
+    RE_SPLIT = '\n[ ]*\n'
+    PARAGRAPH_SEPARAPOR = '\n\n'
+
+    def just(text, width):
+        text = text.replace('\n', ' ')
+        lines = []
+        while True:
+            if len(text) <= width:
+                if not lines:
+                    text = TEXT_INDENT + text
+                lines.append(text)
+                break
+            chunk_end = text.rfind(' ', 0, width + 1)
+            chunk = text[0:chunk_end].strip()
+            chunk_words = re.split('[ ]+', chunk)
+            if not lines:
+                if chunk_words:
+                    chunk_words[0] = TEXT_INDENT + chunk_words[0]
+            words_len = len(''.join(chunk_words))
+            free_space_count = width - words_len
+            interval_count = len(chunk_words) - 1
+            spaces_on_each = free_space_count // interval_count
+            free_space_count -= spaces_on_each * interval_count
+            if free_space_count:
+                offset = (interval_count - free_space_count) // 2
+                while free_space_count:
+                    index = free_space_count + offset
+                    chunk_words[index] += ' '
+                    free_space_count -= 1
+            line = (' ' * spaces_on_each).join(chunk_words)
+            lines.append(line)
+            slice_start = chunk_end + 1
+            text = text[slice_start::]
+        return '\n'.join(lines)
+
+    return PARAGRAPH_SEPARAPOR.join(
+        [just(t, width) for t in re.split(RE_SPLIT, text)])
+
 
 if __name__ == '__main__':
     cmd_parser = ArgumentParser(
@@ -17,7 +56,7 @@ if __name__ == '__main__':
         '-o', '--output', dest='output',
         help='output file', metavar='FILE')
     cmd_parser.add_argument(
-        '-w', '--width', dest='width', default='60',
+        '-w', '--width', dest='width', default=60,
         type=int, help='string width (default: %(default)s)',
         metavar='NUMBER')
     args = cmd_parser.parse_args()
@@ -28,7 +67,8 @@ if __name__ == '__main__':
 
     try:
         with open(args.input) as in_file:
-            pass
+            result = justify(in_file.read(), args.width)
+            print(result)
     except IOError as err:
         print(err)  # hide python traceback
         sys.exit(1)
